3d07ba02a2ecab70f17674c5186a0f4f1bbe9e39
Lyder Stefan Minde Janøy <lyder@animidas.no>
2020-01-31 13:06:50 +0100

    dropped auto-installing Yarn

--- dist/run.sh
+++ dist/run.sh
@@ -7,22 +7,22 @@ function install_node_package {
   yarn global add "$package"
 }
 
-if [ ! ${#macup_node_packages[@]} -eq 0 ]; then # If config has npm entries
-  # Checking if NodeJS is installed
-  if [ ! "$(command -v node)" ]; then # If NodeJS is not installed
-    report_from_package " WARNING: NodeJS is not installed. Installing.."
-    if [ ! "$(command -v brew)" ]; then # If Homebrew is not installed
-      report_from_package " WARNING: Homebrew is not installed. Installing.."
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    brew install node
+function isNpmPackageInstalled() {
+  npm list --depth 1 -g "$1" > /dev/null 2>&1
+}
+
+if [ ! ${#macup_node_packages_yarn[@]} -eq 0 ]; then
+  if isNpmPackageInstalled yarn;then
+    for ((i=0; i<${#macup_node_packages_yarn[@]}; ++i)); do
+      install_node_package "${macup_node_packages_yarn[i]}"
+    done
+    report_from_package " Global NodeJS packages:"
+    yarn global list
+    report_from_package " Outdated global NodeJS packages:"
+    yarn global upgrade-interactive
+  else
+    report_from_package " WARNING: Yarn is not installed. Install Yarn and restart macup."
   fi
-  # Install Node packages
-  for ((i=0; i<${#macup_node_packages[@]}; ++i)); do
-    install_node_package "${macup_node_packages[i]}"
-  done
-  report_from_package " Global NodeJS packages:"
-  yarn global list
-  report_from_package " Outdated global NodeJS packages:"
-  yarn global upgrade-interactive
+else
+  report_from_package " WARNING: No items in the config file"
 fi
